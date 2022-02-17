1daede3eb907dd3fbd98ddb88c603224ecdd4c44
Cookiechan22 <97550005+Cookiechan22@users.noreply.github.com>
2022-02-17 19:33:30 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
+var barco 
+var shipImg1
 
 function preload(){
-
+seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png", "ship-2.png","ship-1.png");
 }
 
 function setup(){
   createCanvas(400,400);
-  
+   barco=createSprite(200,200)
+sea=createSprite(400,200); 
+  sea.addImage(seaImg);
+ sea.velocityX = -5; sea.scale=0.3;
+ship.addAnimation("movingShip",shipImg1); 
+  ship.scale =0.25;
 }
 
 function draw() {
   background("blue");
     drawSprites();
+if(sea.x<0){
+}
+sea.width/2
 
  
 }
